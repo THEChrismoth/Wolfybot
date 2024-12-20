fdb2e4497bdfcd342929751e1b38bc609e62525b
THEChrismoth <30945154+THEChrismoth@users.noreply.github.com>
2024-12-20 22:58:16 +0300

    Initial commit

---
+++ app/config.py
@@ -0,0 +1,19 @@
+from vkbottle import API
+from vkbottle.bot import BotLabeler
+from dotenv import load_dotenv
+import os
+
+# Загрузка переменных окружения из файла .env
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
+# Устанавливаем токен ВКонтакте API
+api = API(os.getenv('VK_TOKEN'))
+labeler = BotLabeler()
+
+# Указываем идентификатор администратора группы VK
+admin_id = os.getenv('ADMIN_ID')
+
+# Указываем авторизационные данные OpenAI
+GPT_key = os.getenv('AITOKEN')
+
+DATABASE_URL = os.getenv("DATABASE_URL", "default_value_if_not_set")
